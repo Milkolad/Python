7a04fd6f9d96fb842672ef8b0aa041d07b211996
Milkolad <milkoladx@gmail.com>
2019-12-03 13:31:50 +0300

    KURSOVAYA

--- Kursovaya.py
+++ Kursovaya.py
@@ -1,10 +1,3 @@
-#a=int(input())
-#b=int(input())
-#c=int(input())
-
-#print(a,b,c)
-
-#class FuzzyTriangular(object):
 
 lst=[]
 print("Введите A")
@@ -19,8 +12,6 @@ for ch in s:
     elif(symb!=""): lst.append(symb); symb="" 
 
 
-print(lst[0],lst[1], lst[2], lst[3], lst[4], lst[5])
-
-"""for a in lst:
-    print(a)"""
+for a in lst:
+    print(a)
     
