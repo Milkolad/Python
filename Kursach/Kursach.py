0eaa55b49ecde4be269a1e901a0fae61414fc98e
Milkolad <milkoladx@gmail.com>
2019-12-04 17:57:44 +0300

    Kursach

--- Kursach/Kursach.py
+++ Kursach/Kursach.py
@@ -27,28 +27,23 @@ def mult(x,y):
             arr[count][1]=y[j]
             arr[count][2]=x[i+1]*y[j+1]
             count+=1
-    print(arr,"\n")
     
     for i in range(lenght):
         arr[i,1]=min(arr[i,0],arr[i,1])
     
     arr=arr[arr[:,2].argsort()]
     arr=arr[:,1:3]
-    print(arr)
-
     res=[]
     myiter=iter(range(lenght))
     for i in myiter:
         if((np.count_nonzero(arr==arr[i][1]))==1): res.append(arr[i][0]); res.append(arr[i][1])
         else: 
-                maximum=arr[i][0]
-                for j in range(np.count_nonzero(arr==arr[i][1])-1):
-                    print(i)
-                    print(arr[i][0])
-                    maximum=max(arr[i][0],maximum)
-                    i=next(myiter,None)
-                res.append(maximum)
-                res.append(arr[i-1][1])
+            maximum=arr[i][0]
+            for j in range(np.count_nonzero(arr==arr[i][1])-1):
+                maximum=max(arr[i][0],maximum)
+                i=next(myiter,None)
+            res.append(maximum)
+            res.append(arr[i-1][1])
     
     return res
 
@@ -69,4 +64,8 @@ def main():
     B=list(map(float,B))
     C=mult(A,B)
     C=list(map(str,C))
+    print("\nОтвет:")
     printf(C)
+
+if __name__== '__main__':
+   main()
