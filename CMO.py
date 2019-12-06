e729fe75232014b662ca871a707a200dc8cab112
Milkolad <milkoladx@gmail.com>
2019-12-06 16:48:27 +0300

    HOMEWORK

--- CMO.py
+++ CMO.py
@@ -1,5 +1,7 @@
 from queue import Queue
+from threading import Thread
 import time
+import random
 
 class Customer(object):
  
@@ -14,8 +16,8 @@ class Customer(object):
         return "Покупатель %s ушел" % self.number
 
     def order(self,q):
-        q.put(self.number)
         if(not(q.empty())): print("Покупатель",self.number,"встает в очередь"); 
+        q.put(self.number)
         return self.number
     
     def wait(self,q):
@@ -29,11 +31,14 @@ class Cashier(object):
         self.name = name
         self.q=q
 
-    def service(self):
-        return  "Кассир %s начинает обслуживать покупателя %s" % (self.name, self.q.get()) 
+    def service(self,number):
+        return  "Кассир %s начинает обслуживать покупателя %s" % (self.name, number) 
+        
 
     def servcomplete(self):
-        return "Кассир %s завершил обслуживание" % self.name
+        amount = random.randint(5, 10)
+        time.sleep(amount)
+        return "Кассир %s завершил обслуживание покупателя %s" % (self.name, self.q.get())
 
     def shout(self):
         return '%s кричит: "Свободная касса!"' % self.name
@@ -54,29 +59,34 @@ class Delivery(object):
         return "Сбор заказа" 
 
     def dev(self):
+        amount = random.randint(5, 10)
+        time.sleep(amount)
         return "Выдача заказа %s покупателю"% self.q.get()
+
+
+def que(number,qc,qd,cash,deliver):
+    cust=Customer(number)
+    print(cust.come())
+    if(qc.empty()):print(cash.shout())
+    cust.order(qc)
+    if(not(qc.empty())): print(cash.service(number)); print(cash.servcomplete()); print(cust.wait(qd))
+    if(not(qd.empty())): print(deliver.collect()); print(deliver.dev()); print(cust.leave())
 
 
 cass=Queue()
 deliv=Queue()
-
 b=Cashier(cass)
 c=Delivery(deliv)
-
-for i in range(1,3):
-    a=Customer(i)
-    print(a.come())
-    if(cass.empty()):print(b.shout())
-    a.order(cass)
-    if(not(cass.empty())):print(b.service()); print(b.servcomplete())
-    
-    
-    #print(a.wait(deliv))
-   # if(not(deliv.empty):print(c.collect())
-    #print(c.dev())
-    #print(a.leave())
-    
+thr1=Thread(target=que, args=(1,cass,deliv,b,c))
+thr2=Thread(target=que, args=(2,cass,deliv,b,c))
 
 
-
-
+thr1.start()
+time.sleep(2)
+thr2.start()
+thr1.join()
+thr2.join()
+#for i in range(1,3):
+    #thr1=Thread(target=que, args=(i,cass,deliv,b,c))
+  #  thr[i-1].start()
+  #  thr[i-1].join()
