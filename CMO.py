9efbca9d87b0d72d1df009382554e469c5844e4f
Milkolad <milkoladx@gmail.com>
2019-12-07 14:13:12 +0300

    HOMEWORK

--- CMO.py
+++ CMO.py
@@ -1,4 +1,5 @@
 from queue import Queue
+from collections import deque
 from threading import Thread
 import time
 import random
@@ -31,14 +32,17 @@ class Cashier(object):
         self.name = name
         self.q=q
 
-    def service(self,number):
-        return  "Кассир %s начинает обслуживать покупателя %s" % (self.name, number) 
-        
+    def service(self):
+        print("Кассир",self.name," начинает обслуживать покупателя")
 
-    def servcomplete(self):
-        amount = random.randint(5, 10)
+    def servcomplete(self,qd):
+        amount = random.randint(4, 7)
         time.sleep(amount)
-        return "Кассир %s завершил обслуживание покупателя %s" % (self.name, self.q.get())
+        self.cu=self.q.get()
+        self.cust=Customer(self.cu)
+        print("Кассир", self.name, "завершил обслуживание покупателя ", self.cu)
+        print((self.cust).wait(qd))
+
 
     def shout(self):
         return '%s кричит: "Свободная касса!"' % self.name
@@ -61,39 +65,47 @@ class Delivery(object):
     def dev(self):
         amount = random.randint(5, 10)
         time.sleep(amount)
-        return "Выдача заказа %s покупателю"% self.q.get()
-
-
-def quecass(cust,qc,qd,cash,deliver):
-    print(cust.come())
-    if(qc.empty()):print(cash.shout())
-    cust.order(qc)
-    if(not(qc.empty())): print(cash.service(cust.number)); print(cash.servcomplete()); print(cust.wait(qd)); 
+        cu=self.q.get()
+        cust=Customer(cu)
+        print("Выдача заказа,покупателю", cu)
+        print(cust.leave())
+        
+        
+def quecass(qc,qd,cash,deliver):
+    while(not(cass.empty())):
+        if(qc.empty()):print(cash.shout())
+        if(not(qc.empty())): cash.service(); cash.servcomplete(qd) 
    
-def quedev(qd,deliver,cust):
-    if (not(qd.empty())): print(deliver.collect()); print(deliver.dev()); print(cust.leave())
+def quedev(qd,deliver):
+    while((not(cass.empty()))|(not(deliv.empty()))):
+        #print(not(cass.empty()))
+        #print(not(deliv.empty()))
+        if (not(qd.empty())): print(deliver.collect()); deliver.dev()
+
+def people(qc,lenght):
+    for i in range(1,lenght+1):
+        cust=Customer(i)
+        print(cust.come())
+        cust.order(qc)
+        amount = random.randint(2, 7)
+        time.sleep(amount)
+
 
 
 cass=Queue()
 deliv=Queue()
 b=Cashier(cass)
 c=Delivery(deliv)
-length=30
+lenght=4 #длина очереди
 
 
-for i in range(1,length+1):
-    cu=Customer(i) 
-    thr1=Thread(target=quecass, args=(cu,cass,deliv,b,c))
-    thr2=Thread(target=quedev, args=(deliv,c,cu))
-    thr1.start()
-    thr2.start()
-    thr1.join()
-    thr2.join()
-    time.sleep(2)
-   
-#костыль
-if(not(deliv.empty())):
-    cus=Customer(length)
-    thr2=Thread(target=quedev, args=(deliv,c,cus))
-    thr2.start()
-    thr2.join()
+thr1=Thread(target=quecass, args=(cass,deliv,b,c))
+thr2=Thread(target=quedev, args=(deliv,c))
+thr3=Thread(target=people, args=(cass,lenght))
+thr3.start()
+time.sleep(0.2)
+thr1.start()
+thr2.start()
+thr1.join()
+thr2.join() 
+thr3.join()
