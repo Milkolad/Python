20a3b93abb9676670978396db23dac035c01fb49
Milkolad <milkoladx@gmail.com>
2019-12-10 20:12:03 +0300

    KR

--- avtomat.py
+++ avtomat.py
@@ -8,21 +8,19 @@ else:
     for i in reversed(range(8)):
         ar[1][i]=str(a%2)
         a=a//2
-    b=[]   
+
+    b=[]  
+    c=[] 
+
     b.append(['0','0','0','0','0','0','0','0','0','0','1','0','0','0','0','0','0','0','0','0','0'])
-    #print(*b[0])
-    c=[]
 
     for k in range(count):
-        c.append(b[k][0])
-        for i in range(len(b[0])-2):
+        for i in range(len(b[0])):
             if(i==0):string="0"+b[k][i]+b[k][i+1]; 
-            else:string=b[k][i]+b[k][i+1]+b[k][i+2]
-            #print(i)
+            elif(i==(len(b[0])-1)): string=b[k][i-1]+b[k][i]+"0"; 
+            else:string=b[k][i-1]+b[k][i]+b[k][i+1]
             for j in range(8):
-                if(string==ar[0][j]): c.append(ar[1][j]); #print(b)
-        c.append(b[k][-1])
-        #print(c)
+                if(string==ar[0][j]): c.append(ar[1][j]);
         b.append(list(c))
         c.clear()
     
